6d210d5f7b6d1879f130e14c55e9cc26d071e240
Edu-Guedes <eduardaguedes062007@gmail.com>
2026-01-29 22:45:02 -0300

    p

--- FinancesMind/assets/JS/script_diario.js
+++ FinancesMind/assets/JS/script_diario.js
@@ -8,6 +8,7 @@ let close = document.querySelector('.close')
 
 menu.addEventListener('click', () => {
     if (nav.getAttribute('class') == 'nav-hidden') {
+        console.log('oi')
         nav.classList.add('nav-active')
         nav.classList.remove('nav-hidden')
     } else {
@@ -24,6 +25,42 @@ close.addEventListener('click', () => {
     }
 
 })
+
+function openButton() {
+    let containerButtons = document.querySelector('.buttons')
+    let floatButton = document.getElementById('float-button')
+    let addTransiction = document.getElementById('add-transaction')
+    let content = document.querySelector('.content')
+
+    containerButtons.classList.remove('buttons')
+    containerButtons.classList.add('open')
+    addTransiction.id = 'open-buttons'
+
+    content.addEventListener('click', (e) => {
+        console.log('oii')
+        console.log(e.target)
+        if (containerButtons.getAttribute('class') === 'open' && e.target !== floatButton) {
+            containerButtons.classList.remove('open')
+            containerButtons.classList.add('buttons')
+            addTransiction.id = 'add-transaction'
+        }
+    })
+
+    addTransiction.addEventListener('click', () => {
+        console.log('olaaaa')
+        if (containerButtons.id === 'open') {
+            containerButtons.classList.remove('open')
+            containerButtons.classList.add('buttons')
+            addTransiction.id = 'add-transaction'
+        } else {
+            containerButtons.classList.remove('buttons')
+            containerButtons.classList.add('open')
+            addTransiction.id = 'open-buttons'
+        }
+    })
+
+}
+
 
 
 // ---------------- DIARIO JS --------------------------------------------------------------------------------------------
